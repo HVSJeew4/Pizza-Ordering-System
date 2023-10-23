982b8510bd10ef2103222ff9b3880e2572c12685
Subhash Jeewantha <126590375+HVSJeew4@users.noreply.github.com>
2023-10-23 10:50:46 +0530

    Add project files.

---
+++ PizzaOrderingForm01/Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace PizzaOrderingForm01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.price_box1 = new System.Windows.Forms.TextBox();
+            this.quantityB1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TotalB1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.subTotal = new System.Windows.Forms.TextBox();
+            this.paidbox = new System.Windows.Forms.TextBox();
+            this.refundbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(174, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.label1.Size = new System.Drawing.Size(786, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pizza Ordering Managment System";
+            this.label1.UseMnemonic = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(36, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Pizza Size";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Small",
+            "Medium",
+            "Large"});
+            this.comboBox1.Location = new System.Drawing.Point(141, 181);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(152, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(27, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Pizza Price";
+            // 
+            // price_box1
+            // 
+            this.price_box1.Location = new System.Drawing.Point(141, 228);
+            this.price_box1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.price_box1.Name = "price_box1";
+            this.price_box1.Size = new System.Drawing.Size(152, 22);
+            this.price_box1.TabIndex = 4;
+            // 
+            // quantityB1
+            // 
+            this.quantityB1.Location = new System.Drawing.Point(141, 270);
+            this.quantityB1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.quantityB1.Name = "quantityB1";
+            this.quantityB1.Size = new System.Drawing.Size(152, 22);
+            this.quantityB1.TabIndex = 6;
+            this.quantityB1.TextChanged += new System.EventHandler(this.quantityB1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(45, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 19);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Quantity";
+            // 
+            // TotalB1
+            // 
+            this.TotalB1.Location = new System.Drawing.Point(141, 313);
+            this.TotalB1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TotalB1.Name = "TotalB1";
+            this.TotalB1.Size = new System.Drawing.Size(152, 22);
+            this.TotalB1.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label6.Location = new System.Drawing.Point(45, 315);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 19);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Total";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(363, 177);
+            this.listView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(655, 221);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Type";
+            this.columnHeader1.Width = 84;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Prise";
+            this.columnHeader2.Width = 87;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Quantity";
+            this.columnHeader3.Width = 117;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Total";
+            this.columnHeader4.Width = 161;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RosyBrown;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.button1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Brown;
+            this.button1.Location = new System.Drawing.Point(80, 365);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 33);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Save and Print";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(885, 133);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(132, 22);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.RosyBrown;
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.button2.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Brown;
+            this.button2.Location = new System.Drawing.Point(735, 126);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 34);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.RosyBrown;
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.button3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Brown;
+            this.button3.Location = new System.Drawing.Point(80, 415);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(195, 34);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Set the Prices";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.RosyBrown;
+            this.button4.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.button4.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Brown;
+            this.button4.Location = new System.Drawing.Point(317, 415);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 34);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Sub Total";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.RosyBrown;
+            this.button5.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.button5.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.Brown;
+            this.button5.Location = new System.Drawing.Point(553, 416);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 34);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Paid";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.RosyBrown;
+            this.button6.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.button6.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.Brown;
+            this.button6.Location = new System.Drawing.Point(788, 415);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(99, 34);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "Refund";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // subTotal
+            // 
+            this.subTotal.Location = new System.Drawing.Point(423, 422);
+            this.subTotal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.subTotal.Name = "subTotal";
+            this.subTotal.Size = new System.Drawing.Size(124, 22);
+            this.subTotal.TabIndex = 18;
+            // 
+            // paidbox
+            // 
+            this.paidbox.Location = new System.Drawing.Point(659, 422);
+            this.paidbox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.paidbox.Name = "paidbox";
+            this.paidbox.Size = new System.Drawing.Size(124, 22);
+            this.paidbox.TabIndex = 19;
+            // 
+            // refundbox
+            // 
+            this.refundbox.Location = new System.Drawing.Point(893, 422);
+            this.refundbox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.refundbox.Name = "refundbox";
+            this.refundbox.Size = new System.Drawing.Size(124, 22);
+            this.refundbox.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 553);
+            this.Controls.Add(this.refundbox);
+            this.Controls.Add(this.paidbox);
+            this.Controls.Add(this.subTotal);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.TotalB1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.quantityB1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.price_box1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox price_box1;
+        private System.Windows.Forms.TextBox quantityB1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TotalB1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox subTotal;
+        private System.Windows.Forms.TextBox paidbox;
+        private System.Windows.Forms.TextBox refundbox;
+    }
+}
+
---
+++ PizzaOrderingForm01/Form1.cs
@@ -0,0 +1,99 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Management;
+using System.Windows.Forms;
+namespace PizzaOrderingForm01
+{
+    public partial class Form1 : Form
+    {
+        public MySqlConnection con = new MySqlConnection();
+        MySqlCommand cmd;
+
+        public Form1()
+        {
+            InitializeComponent();
+            con.ConnectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=pizza_db;";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string query = ""; // Define the query based on selected size
+
+            if (comboBox1.SelectedItem.ToString() == "Small")
+            {
+                query = "SELECT Prices FROM pizza_prices WHERE ID = 1";
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Medium")
+            {
+                query = "SELECT Prices FROM pizza_prices WHERE ID = 2";
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Large")
+            {
+                query = "SELECT Prices FROM pizza_prices WHERE ID = 3";
+            }
+            else
+            {
+                price_box1.Text = "0";
+                return; // Exit if no valid size is selected
+            }
+
+            using (MySqlCommand cmd = new MySqlCommand(query, con))
+            {
+                con.Open();
+                try
+                {
+                    Int32 fun = Convert.ToInt32(cmd.ExecuteScalar());
+                    price_box1.Text = fun.ToString();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+            con.Close(); // Close the connection
+        }
+
+        private void quantityB1_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(price_box1.Text, out int price) && int.TryParse(quantityB1.Text, out int quantity))
+            {
+                TotalB1.Text = (price * quantity).ToString();
+            }
+            else
+            {
+                TotalB1.Text = ""; // Clear the total value if the input is not valid
+            }
+
+            // Clear the other text boxes
+         
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //this function show the sub total
+            //the code for the add data to list view
+            if (TotalB1.Text.Length > 0)
+            {
+                string[] arr = new string[4];
+                arr[0] = comboBox1.Text;
+                arr[1] = price_box1.Text;
+                arr[2] = quantityB1.Text;
+                arr[3] = TotalB1.Text;
+                ListViewItem item = new ListViewItem(arr);
+                listView1.Items.Add(item);
+                //this function show the sub total
+
+                int x = int.Parse(quantityB1.Text);
+                int y = int.Parse(price_box1.Text);
+
+
+                subTotal.Text = ( x * y ).ToString();
+
+            }
+            price_box1.Text = "";
+            quantityB1.Text = "";
+        }
+    }
+    }
+
